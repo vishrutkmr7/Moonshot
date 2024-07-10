99b5359aa1863fe52a7ac49d23ce14193c29aca0
Vishrut Jha <vishrutkmr7@icloud.com>
2024-07-10 00:02:01 -0700

    Initial Commit

---
+++ MoonshotTests/MoonshotTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotTests.swift
+//  MoonshotTests
+//
+//  Created by Vishrut Jha on 7/10/24.
+//
+
+import Testing
+@testable import Moonshot
+
+struct MoonshotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
